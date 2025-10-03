206563857d81e83290b84579b91002dd83294993
Alex Myhill <adcm2@cam.ac.uk>
2025-10-03 16:11:24 +0100

    docs: Generate and configure Doxygen for GitHub Pages

---
+++ docs/search/all_0.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['clean_5fbench_5f1_2ecpp_0',['clean_bench_1.cpp',['../clean__bench__1_8cpp.html',1,'']]],
+  ['clean_5fbench_5f10_2ecpp_1',['clean_bench_10.cpp',['../clean__bench__10_8cpp.html',1,'']]],
+  ['clean_5fbench_5f2_2ecpp_2',['clean_bench_2.cpp',['../clean__bench__2_8cpp.html',1,'']]],
+  ['clean_5fbench_5f3_2ecpp_3',['clean_bench_3.cpp',['../clean__bench__3_8cpp.html',1,'']]],
+  ['clean_5fbench_5f4_2ecpp_4',['clean_bench_4.cpp',['../clean__bench__4_8cpp.html',1,'']]],
+  ['clean_5fbench_5f5_2ecpp_5',['clean_bench_5.cpp',['../clean__bench__5_8cpp.html',1,'']]],
+  ['clean_5fbench_5f6_2ecpp_6',['clean_bench_6.cpp',['../clean__bench__6_8cpp.html',1,'']]],
+  ['clean_5fbench_5f7_2ecpp_7',['clean_bench_7.cpp',['../clean__bench__7_8cpp.html',1,'']]],
+  ['clean_5fbench_5f8_2ecpp_8',['clean_bench_8.cpp',['../clean__bench__8_8cpp.html',1,'']]],
+  ['clean_5fbench_5f9_2ecpp_9',['clean_bench_9.cpp',['../clean__bench__9_8cpp.html',1,'']]]
+];
